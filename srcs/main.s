46292fa2a1b3d3d0d7cf91796fbb83f635821c09
Jeremy Maia <jmaia@student.42.fr>
2024-10-19 21:06:24 +0200

    Fix compilation

--- srcs/main.s
+++ srcs/main.s
@@ -13,6 +13,9 @@
 %define	PROT_READ			0x1
 %define	PROT_WRITE			0x2
 %define	MAP_SHARED 			0x01
+%define	MAP_PRIVATE			0x02
+%define	MAP_FIXED			0x10
+%define	MAP_ANONYMOUS			0x20
 %define	MMAP_ERRORS			-4095
 
 %define BUFFER_SIZE 1024
